61843a347d3f3ac8d09fffa0a150b84dce400d37
Snappye <63756470+Snappye@users.noreply.github.com>
2020-04-16 01:07:56 +0200

    Add files via upload

---
+++ About_Me/src/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      speed: 1000,
+      dots: true,
+      waitForAnimate: false,
+
+    });
+    
+  });
